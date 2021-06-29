0995ef7d287e2f0a251d603b9ab72eeb17e12fc5
Nathan68190 <55447407+Nathan68190@users.noreply.github.com>
2021-06-29 14:22:17 +0200

    Add files via upload

---
+++ myApp/src/test-difficulty.jsx
@@ -0,0 +1,22 @@
+function difficulty(difficulte) {
+    let health;
+    let divide = health / level
+    let time; 
+    let divide = time / level;
+    switch (difficulte) {
+        case "easy" : 
+        health = 100;
+        time = 60;
+        break;
+
+        case "medium" : 
+        health = 60;
+        time = 40;
+        break;
+
+        case "hard" : 
+        health = 40 
+        time = 20
+        break;
+    }
+}
